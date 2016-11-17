61adc991cae7d9e898e891d2bc0a3e6f96360d30
Clementina Calvo <cc@eiwa.ag>
2016-11-17 13:33:49 -0300

    Change speed-up percentage

--- rocket.c
+++ rocket.c
@@ -1,8 +1,17 @@
+/*
+ * Rocket
+ * Author: Clementina Calvo <ccalvo@itba.edu.ar>
+ * Date: 11/2016
+ *
+ */
+
 #include <stdio.h>
 #include <unistd.h> // for usleep function
 
 #define MAX_CONSOLE_LINES       50
 #define START_SLEEP_TIME_US     300000
+#define SPEED_UP_PERCENTAGE     10
+#define SPEED_UP_CONSTANT       (100-SPEED_UP_PERCENTAGE)/100
 
 const char rocket[] =
 "           _\n\
@@ -23,15 +32,14 @@ int main()
 {
     int i;
     for (i = 0; i < MAX_CONSOLE_LINES; i ++)
-        printf("\n"); // jump to bottom of console
+        printf("\n");                       // jump to bottom of console
 
     printf("%s", rocket);
 
     int j = START_SLEEP_TIME_US;
     for (i = 0; i < MAX_CONSOLE_LINES; i ++) {
-        usleep(j); // move faster and faster,
-        j = (int)(j * 0.9); // so sleep less each time
-        printf("\n"); // move rocket a line upward
+        usleep(j);                          // move faster and faster,
+        j = (int)(j * SPEED_UP_CONSTANT);   // so sleep less each time
     }
     return 0;
 }
